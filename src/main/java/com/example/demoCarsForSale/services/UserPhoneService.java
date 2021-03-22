0d7d8aebcfce8b7c88158ec57e31da570afe8189
Sharyf <xandrey95x@live.ru>
2021-03-22 11:39:33 +0300

    corrected phoneService

--- src/main/java/com/example/demoCarsForSale/services/UserPhoneService.java
+++ src/main/java/com/example/demoCarsForSale/services/UserPhoneService.java
@@ -2,12 +2,11 @@ package com.example.demoCarsForSale.services;
 
 import com.example.demoCarsForSale.dao.model.UserPhone;
 
-import java.io.Serializable;
 import java.util.List;
 
 public interface UserPhoneService {
 
     List<UserPhone> save(List<UserPhone> phones);
 
-    List<UserPhone> get(Serializable userID);
+    List<UserPhone> get(Object userID);
 }
--- src/main/java/com/example/demoCarsForSale/services/impl/UserPhoneServiceHandler.java
+++ src/main/java/com/example/demoCarsForSale/services/impl/UserPhoneServiceHandler.java
@@ -8,7 +8,6 @@ import com.example.demoCarsForSale.exeptions.InternalErrorException;
 import com.example.demoCarsForSale.services.UserPhoneService;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -18,7 +17,10 @@ public class UserPhoneServiceHandler extends AbstractService implements UserPhon
     @Override
     public List<UserPhone> save(List<UserPhone> phones) {
         try {
-            return USER_PHONE_DAO.save(phones);
+            List<UserPhone> userPhones = USER_PHONE_DAO.save(phones);
+            commit();
+
+            return userPhones;
         } catch (SQLException e) {
             rollback();
             throw new BadRequestException(e.getMessage(), e, HttpServletResponse.SC_BAD_REQUEST);
@@ -26,9 +28,12 @@ public class UserPhoneServiceHandler extends AbstractService implements UserPhon
     }
 
     @Override
-    public List<UserPhone> get(Serializable userID) {
+    public List<UserPhone> get(Object userID) {
         try {
-            return USER_PHONE_DAO.get(userID);
+            List<UserPhone> userPhones = USER_PHONE_DAO.get(userID);
+            commit();
+
+            return userPhones;
         } catch (SQLException e) {
             rollback();
             throw new InternalErrorException("Oops, something went wrong while fetching Phones", e, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
